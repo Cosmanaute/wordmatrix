6bc6a96955bde82b1fda8fd015de8f0ed39772c1
cosmanaute <cosmanaute@gmail.com>
2023-08-19 13:20:36 +0200

    added diagonal and reverse search

--- main.c
+++ main.c
@@ -92,7 +92,73 @@ int locateWords(char matrix[WIDTH][HEIGHT]) {
         }
     }
 
-    printf("________\n");
+    fseek(f, 0, SEEK_SET);
+    printf("\n\n______________\nDIAGONAL(Left)\n\n");
+
+    // diagonal left
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = WIDTH - 1; x >= 0; --x) {
+            int i = 0;
+            while (i <= x && i <= HEIGHT - y - 1) {
+                int size;
+                if (y > WIDTH - 1 - x) size = WIDTH - 1 - x;
+                else size = y;
+
+                char buffer[size + 1];
+                buffer[i] = matrix[y + i][x - i]; // Left diagonal indexing
+                buffer[i + 1] = '\0';
+                char line[24];
+                while (fgets(line, sizeof(line), f) != NULL) {
+                    line[strcspn(line, "\n")] = '\0';
+                    strToUpper(line);
+                    if (strcmp(buffer, line) == 0) {
+                        printf("%s @ %d:%d\n", buffer, y + 1, x + 1);
+                    }
+                    reverse(buffer);
+                    if (strlen(buffer) != 1 && strcmp(buffer, line) == 0) {
+                        printf("%s @ %d:%d\n", buffer, y + 1, x + 1);
+                    }
+                } 
+
+                fseek(f, 0, SEEK_SET);
+                ++i;
+            }
+        }
+    }
+
+    fseek(f, 0, SEEK_SET);
+    printf("\n\n______________\nDIAGONAL(Right)\n\n");
+
+    // diagonal right 
+    for (int y = 0; y < HEIGHT; ++y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            int i = 0;
+            while (i <= WIDTH - 1 - x && i <= HEIGHT - y - 1) {
+                int size;
+                if (y > x) size = x;
+                else size = y;
+
+                char buffer[size + 1];
+                buffer[i] = matrix[y + i][x + i]; 
+                buffer[i + 1] = '\0';
+                char line[24];
+                while (fgets(line, sizeof(line), f) != NULL) {
+                    line[strcspn(line, "\n")] = '\0';
+                    strToUpper(line);
+                    if (strcmp(buffer, line) == 0) {
+                        printf("%s @ %d:%d\n", buffer, y + 1, x + 1);
+                    }
+                    reverse(buffer);
+                    if (strlen(buffer) != 1 && strcmp(buffer, line) == 0) {
+                        printf("%s @ %d:%d\n", buffer, y + 1, x + 1);
+                    }
+                } 
+
+                fseek(f, 0, SEEK_SET);
+                ++i;
+            }
+        }
+    }
 
     fclose(f);
     return 0;
